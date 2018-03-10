3ab5ec821cb8584be9f2360e3d73ec3b602c5185
jairo <jairo.goncalves90@gmail.com>
2018-03-10 12:15:16 -0300

    Exercicios 1 ao 5

--- src/App.js
+++ src/App.js
@@ -1,19 +1,64 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import Cabecalho from './components/Cabecalho'
+import Dashboard from './components/Dashboard'
+import Widget from './components/Widget'
+import TrendsArea from './components/TrendsArea'
+import Tweet from './components/Tweet'
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      novoTweet: ''
+    }
+    console.log('MY EGGS',this.state.novoTweet.length)
+  }
+
+  isTweetValid(){
+    return this.state.novoTweet.length > 140 
+  }
   render() {
+    
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Fragment>
+        <Cabecalho usuario="@omariosouto" />
+        <div className="container">
+            <Dashboard>
+                <Widget>
+                    <form className="novoTweet">
+                        <div className="novoTweet__editorArea">
+                            <span 
+                              className={
+                                `novoTweet__status 
+                                ${this.isTweetValid() ? 'novoTweet__status--invalido' : ''}
+                                `}>{this.state.novoTweet.length}/140</span>
+                            <textarea className="novoTweet__editor"
+                              onChange = {(event)=> {this.setState({novoTweet: event.target.value})}} 
+                              placeholder="O que está acontecendo?" 
+                              value={this.state.novoTweet}>
+                            </textarea>
+                        </div>
+                        <button
+                          disabled={ this.isTweetValid() ? true : false}
+                          type="submit" 
+                          className="novoTweet__envia">
+                          Tweetar
+                        </button>
+                    </form>
+                </Widget>
+                <Widget>
+                    <TrendsArea />
+                </Widget>
+            </Dashboard>
+            <Dashboard posicao="centro">
+                <Widget>
+                    <div className="tweetsArea">
+                        <Tweet />
+                    </div>
+                </Widget>
+            </Dashboard>
+        </div>
+      </Fragment>
     );
   }
 }
